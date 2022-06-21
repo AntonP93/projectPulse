7de0851d5e6f38a36b07d02f4a2c5881a5a07480
Anton Plastun <p.anton070793@gmail.com>
2022-06-21 11:32:18 +0300

    fix: img bag

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         infinite: true,
         speed: 1000,
         adaptiveHeight: true,
-        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/slide/arrow_l.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slide/arrow_r.svg"></button>',
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/slide/arrow_l.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slide/arrow_r.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
